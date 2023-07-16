370ff046a157f5a9a3878ab04acb5e36f76532b7
matteutsch <matteutsch@gmail.com>
2023-07-16 18:07:15 +0200

    added fullscreen mode; repositioned buttons

--- js/game.js
+++ js/game.js
@@ -4,6 +4,7 @@ let startScreen;
 let world;
 let keyboard = new Keyboard();
 let lastInteractionTime = new Date().getTime();
+let fullscreenMode = false;
 
 /**
  * Clears all active intervals.
@@ -23,10 +24,14 @@ function init() {
   muteAll();
 }
 
+/**
+ * Handles the initialization of the game.
+ */
 function handleInit() {
   endScreen = document.getElementById("end-screen");
   startScreen = document.getElementById("start-screen");
-  document.getElementById("mute").classList.remove("d-none");
+  document.getElementById("canvas-container").classList.remove("d-none");
+  document.getElementById("mute-fs").classList.remove("d-none");
   startScreen.classList.add("d-none");
   canvas.classList.remove("d-none");
   endScreen.classList.add("d-none");
@@ -65,6 +70,75 @@ function checkDeviceOrientation() {
 }
 
 /**
+ * Toggles the fullscreen mode of the game.
+ */
+function toggleFullscreen() {
+  let fullscreen = document.getElementById("fullscreen");
+  if (!fullscreenMode) {
+    openFullscreen(fullscreen);
+    fullscreenMode = true;
+  } else {
+    closeFullscreen();
+    fullscreenMode = false;
+  }
+  toggleFullscreenImage();
+}
+
+/**
+ * Opens the fullscreen mode for the specified element.
+ *
+ * @param {HTMLElement} elem - The element to open in fullscreen mode.
+ */
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+  extendCanvas();
+}
+
+/**
+ * Closes the fullscreen mode.
+ */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
+    document.msExitFullscreen();
+  }
+  shrinkCanvas();
+}
+
+/**
+ * Extends the canvas size for fullscreen mode.
+ */
+function extendCanvas() {
+  document
+    .getElementById("canvas-container")
+    .classList.add("extend-canvas-container");
+  document.getElementById("canvas").classList.add("extend-canvas");
+}
+
+/**
+ * Shrinks the canvas size after exiting fullscreen mode.
+ */
+function shrinkCanvas() {
+  document
+    .getElementById("canvas-container")
+    .classList.remove("extend-canvas-container");
+  document.getElementById("canvas").classList.remove("extend-canvas");
+}
+
+/**
  * Toggles the mute state of the game.
  */
 function toggleMute() {
@@ -78,9 +152,21 @@ function toggleMute() {
 function toggleMuteImage() {
   let muteImg = document.getElementById("mute");
   if (!mute) {
-    muteImg.src = "img/6.Botones/mute.png";
+    muteImg.src = "img/6.Botones/mute2.png";
   } else {
-    muteImg.src = "img/6.Botones/unmute.png";
+    muteImg.src = "img/6.Botones/speaker2.png";
+  }
+}
+
+/**
+ * Toggles the fullscreen image.
+ */
+function toggleFullscreenImage() {
+  let fsImg = document.getElementById("enterFS");
+  if (!fullscreenMode) {
+    fsImg.src = " img/6.Botones/fullscreen_enter.png";
+  } else {
+    fsImg.src = "img/6.Botones/fullscreen_exit.png";
   }
 }
 
@@ -100,8 +186,16 @@ function showEndScreen(img) {
   endScreen.classList.remove("d-none");
   canvas.classList.add("d-none");
   endScreen.style.backgroundImage = `url('${img}')`;
-  let hud = document.getElementById("hud");
-  hud.classList.add("d-none");
+  document.getElementById("canvas-container").classList.add("d-none");
+  hideButtons();
+}
+
+/**
+ * Hides Mute, Fullscreen and Hud Icons
+ */
+function hideButtons() {
+  document.getElementById("hud").classList.add("d-none");
+  document.getElementById("mute-fs").classList.add("d-none");
 }
 
 /**
@@ -113,8 +207,7 @@ function backToMenu() {
   startScreen.classList.remove("d-none");
   startContainer.classList.remove("d-none");
   document.getElementById("rotate").classList.add("d-none");
-  document.getElementById("mute").classList.add("d-none");
-  muteAll();
+  document.getElementById("canvas-container").classList.add("d-none");
 }
 
 /**
